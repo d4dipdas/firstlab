617ed47a94fdcf1b5a227ccbbea0d4c0b1fd4a9f
Sachin Parmar <103635352+sachin9603@users.noreply.github.com>
2024-11-19 11:40:16 +0530

    Add files via upload

---
+++ Basic_02.py
@@ -0,0 +1,53 @@
+"""a = int(input("Enter a :" ) ) ;
+b  = int(input ( "Enter b :") ) ;
+c = int( input("Enter C : ")) ;
+if a>b and a>c :
+    print("From the above three numbers given a is a largest" ) ;
+if b>a and c<b :
+    print ("b is the largestt " ) ;
+if c>b and c>a :
+    print ("c is largest " ) ;  """
+
+# example for if /else
+
+""" age = int(input( " Enter your age" )) ;
+
+if age >= 18 :
+    print("you are eligible for vote " ) ;
+else :
+    print("sorry! you have to wait " ) ; """
+
+
+# elseif or elif
+
+""" num = int(input("Enter the number?"))    
+if num==10:    
+    print("The given number is equals to 10")    
+elif num==50:  
+    print("The given number is equal to 50");    
+elif num==100:    
+    print("The given number is equal to 100");    
+else:    
+    print("The given number is not equal to 10, 50 or 100");    """
+
+
+# for loop in python
+
+
+numbers  = [1,3,2,4,5] ;
+total_sum  = 0  ;
+
+for num in numbers :
+
+    total_sum = total_sum + num**2  ;
+
+print("Sum of Square is : " ,total_sum ) ;
+    
+    
+
+
+        
+
+
+
+           
