28e67b2550c797fc9aa3ddfa633dbebc240c3494
sachin9603 <sachinparmar3803@gmail.com>
2024-12-20 11:24:40 +0530

    sachin

---
+++ Analysis.py
@@ -0,0 +1,25 @@
+# pandas if you want to modifiy data and read data from eternal data you can use panadas 
+
+# when u install pandas numpy will get numpy automatically 
+
+
+
+# matplotlib   seaborn  for data visuvalization  
+#seaborn for scintific data visulization 
+
+# scikit-learn for prediction and machine learning 
+#scipy for scitific computing 
+#---------------------------------------------for website ---------------
+#WEPP -- web application using paython
+#django 
+# flask 
+# streamlit 
+#  
+# ---------------------------------------------------------
+# if you want to analyis data you need to create a environment 
+# it is like virtual space 
+# what is advantage of createing virtual environment where i am my dependency while shareing code you no need to install all dependency sepratly ----Testing deprt will directly install this folder and can start 
+
+
+
+
---
+++ Tupleex.py
@@ -0,0 +1,37 @@
+mytuple =  (1,2,3,4,5,6,7,8,9,10)
+
+# tuple performance is faster thna list 
+# we cannot store any duplicate value 
+# it does not allow duplicate 
+# its immutable , if you want to store coding number 
+# it is more memory efficient  
+# tuple is  more secure 
+# its more readble  
+# is 
+
+x  = [
+
+    (12.232, 232.232 , 232.23 ) , 
+   (12.232, 232.232 , 232.23 ) 
+
+]
+
+# real life example 
+employee = ("John Doe", 30, "Software Engineer", "New York")  
+
+# Tuple representing an employee
+
+
+
+
+
+
+print(mytuple)
+
+# how to iterate using for loop 
+for i in mytuple :
+    print(i**2) 
+
+# print dir of Tuple 
+print(dir(mytuple))
+
